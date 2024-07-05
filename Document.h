fcb6efb51983cd7ea0c04b99515a62a1c4117cd0
vaishnavib2205 <vaishnavii.b.bhave@gmail.com>
2024-07-06 02:16:39 +0530

    started

---
+++ Document.h
@@ -0,0 +1,17 @@
+// Document.h
+
+#define DOCUMENT_H
+
+#include <string>
+
+class Document {
+private:
+    std::string title;
+    std::string content;
+
+public:
+    Document(const std::string& title, const std::string& content);
+    std::string getTitle() const;
+    std::string getContent() const;
+};
+
---
+++ Index.h
@@ -0,0 +1,9 @@
+#define INDEX_H
+#include <unordered_map>
+#include<memory>
+#include "Document.h"
+
+class Index{
+    private:
+    unordered_map<string, shared_ptr
+};
